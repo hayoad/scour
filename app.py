bda50615cac42a03050870a84ae9ef12ec6a2e5d
Ayomide Adeniyi <ayomadeniyi@outlook.com>
2024-11-26 18:55:14 +0100

    modified:   __pycache__/app.cpython-313.pyc 	modified:   app.py 	modified:   instance/blog.db 	modified:   requirements.txt

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, render_template, request, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate   
+from flask_sqlalchemy import SQLAlchemy 
+
 from datetime import datetime
 
 app = Flask(__name__)
@@ -9,7 +9,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+
 
 class BlogPost(db.Model):
     id = db.Column(db.Integer, primary_key=True)
